9105c92d9f1ee0a9061a4825cc7b5eaba7e9a03e
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2025-12-04 23:33:13 +0900

    feat: mute_mobileのUser-Agent設定を実装

--- src/xrayproxy/config/rewrite.py
+++ src/xrayproxy/config/rewrite.py
@@ -25,6 +25,7 @@ class RewriteConfig:
     dummy_favicon: bool
     cache_max_age: int
     mute_mobile: bool
+    mute_mobile_options: "MuteMobileOptions"
     mute_enemy_voice: "MuteEnemyVoiceConfig"
     replace_ship_graphics: "ReplaceShipGraphicConfig"
 
@@ -38,6 +39,17 @@ class MuteEnemyVoiceConfig:
     enabled: bool
     ship_ids: tuple[str, ...]
     voice_types: tuple[str, ...]
+
+
+@dataclass(frozen=True, kw_only=True, slots=True, eq=False)
+class MuteMobileOptions:
+    """
+    Optional Configuration of mute for mobile browsers
+    """
+
+    safari: bool
+    firefox: bool
+    user_agent: Optional[str]
 
 
 @dataclass(frozen=True, kw_only=True, slots=True, eq=False)
@@ -62,6 +74,7 @@ class ReplaceShipGraphicConfig:
 
 
 def create_rewrite_config(data: dict[str, Any]) -> RewriteConfig:
+    mute_mobile_user_agent = data.get("mute_mobile_user_agent")
     replace_ship_graphic_entries = tuple(
         create_replace_ship_graphic_entry(raw_entry) for raw_entry in data.get("replace_ship_graphic", [])
     )
@@ -70,6 +83,11 @@ def create_rewrite_config(data: dict[str, Any]) -> RewriteConfig:
         dummy_favicon=bool(data.get("dummy_favicon", False)),
         cache_max_age=int(data.get("cache_max_age", 14400)),
         mute_mobile=bool(data.get("mute_mobile", False)),
+        mute_mobile_options=MuteMobileOptions(
+            safari=bool(data.get("mute_mobile_safari", False)),
+            firefox=bool(data.get("mute_mobile_firefox", False)),
+            user_agent=str(mute_mobile_user_agent) if mute_mobile_user_agent else None,
+        ),
         mute_enemy_voice=create_mute_boss_voice_config(data.get("mute_enemy_voice", {})),
         replace_ship_graphics=ReplaceShipGraphicConfig(
             mapping={entry.from_ship_id: entry for entry in replace_ship_graphic_entries}
